4d24258bea76d2b78080b8855ad5d862f67e2821
sureng24 <tnfustnfus77@naver.com>
2024-11-08 20:03:35 +0900

    [파일 추가] resources 폴더 생성, DDL 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+create database board default character set utf8;
+grant all privileges on board.* to 'jspexam'@'localhost';
+create table board.member (
+	memberid varchar(50) primary key,
+    name varchar(50) not null,
+    password varchar(10) not null,
+    regdate datetime not null
+) engine=InnoDB default character set = utf8;
+create table board.article (
+	article_no int auto_increment primary key,
+    writer_id varchar(50) not null,
+    writer_name varchar(50) not null,
+    title varchar(255) not null,
+    regdate datetime not null,
+    moddate datetime not null,
+    read_cnt int
+) engine=InnoDB default character set = utf8;
+create table board.article_content (
+	article_no int primary key,
+    content text
+) engine=InnoDB default character set = utf8;
