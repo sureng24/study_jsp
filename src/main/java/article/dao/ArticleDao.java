81b91eb7d20ae9461adbb44bc56321b66b73ace5
sureng24 <tnfustnfus77@naver.com>
2024-11-10 15:30:48 +0900

    [기능 추가] 게시글 목록 조회

--- src/main/java/article/dao/ArticleDao.java
+++ src/main/java/article/dao/ArticleDao.java
@@ -6,9 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import article.model.Article;
+import article.model.Writer;
 import jdbc.JdbcUtil;
 
 public class ArticleDao {
@@ -51,6 +54,57 @@ public class ArticleDao {
 	
 	private Timestamp toTimestamp(Date date) {
 		return new Timestamp(date.getTime());
+	}
+	
+	public int selectCount(Connection conn) throws SQLException {
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select count(*) from article");
+			if (rs.next()) {
+				return rs.getInt(1);
+			}
+			return 0;
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(stmt);
+		}
+	}
+	
+	public List<Article> select(Connection conn, int startRow, int size) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement("select * from article " +
+					"order by article_no desc limit ?, ?");
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, size);
+			rs = pstmt.executeQuery();
+			List<Article> result = new ArrayList<>();
+			while (rs.next()) {
+				result.add(convertArticle(rs));
+			}
+			return result;
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+	
+	private Article convertArticle(ResultSet rs) throws SQLException {
+		return new Article(rs.getInt("article_no"),
+				new Writer(
+						rs.getString("writer_id"),
+						rs.getString("writer_name")),
+				rs.getString("title"),
+				toDate(rs.getTimestamp("regdate")),
+				toDate(rs.getTimestamp("moddate")),
+				rs.getInt("read_cnt"));
+	}
+	
+	private Date toDate(Timestamp timestamp) {
+		return new Date(timestamp.getTime());
 	}
 
 }
